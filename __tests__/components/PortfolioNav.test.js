7ccaadb4809f58e20bf5d1a8e89b695f6ba2be76
james gladden <jamesgladden@gmail.com>
2018-10-09 12:13:35 -0700

    portoflio nav button tests

--- __tests__/components/PortfolioNav.test.js
+++ __tests__/components/PortfolioNav.test.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import { shallow, render, mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import PortfolioNav from '../../src/components/PortfolioNav';
 import Button from '../../src/components/button';
 
 const props = {
-  portfolioTypes: ['online'],
-  setPortfolioType: () => {}
+  portfolioTypes: ['online', 'print'],
+  setPortfolioType: type => type
 }
 
-describe('<PortfolioNav />', () => {
+describe('Portfolio Nav', () => {
+  const portfolionav = mount(<PortfolioNav {...props} />);
   test('renders the component', () => {
-    const wrapper = shallow(<PortfolioNav {...props} />);
-    expect(wrapper.exists()).toBe(true);
+    expect(portfolionav.exists()).toBe(true);
   });
-  test('renders buttons using portfolioTypes values', () => {
-    const wrapper = mount(<PortfolioNav {...props} />);
-    wrapper.setProps({
-        portfolioTypes: ['online', 'print']
-    });
-    expect(wrapper.find('button')).toHaveLength(2);
+  test('renders buttons for each portfolio type', () => {
+    expect(portfolionav.find('button')).toHaveLength(2);
   });
 });
 
+describe('Portfolio Nav Button', () => {
+  const type = props.portfolioTypes[0];
+  const func = () => props.setPortfolioType({type});
+  const mockOnClick = jest.fn(func);
+  const navbutton = mount(
+    <Button
+      handleClick={mockOnClick}
+      label={type}
+    />
+  );
+  test('renders the component', () => {
+    expect(navbutton.exists()).toBe(true);
+  });
+  test('onClick returns expected payload', () => {
+    navbutton.find('button').simulate('click');
+    expect(mockOnClick.mock.results[0].value).toEqual({type});
+  });
+});
