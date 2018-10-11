7b161cb696e16a26650de9a160c5fc6ccbec6769
james gladden <jamesgladden@gmail.com>
2018-10-10 18:11:46 -0700

    refactored portfolio item test

--- __tests__/components/PortfolioItem.test.js
+++ __tests__/components/PortfolioItem.test.js
@@ -5,19 +5,18 @@ import PortfolioItem from '../../src/components/PortfolioItem';
 const props = {
   title: 'title',
   description: 'description',
-  handleClick: id => id
-}
+};
 
 describe('Portfolio Item', () => {
+  const id = { id: '22'};
+  const func = () => id;
+  const mockOnClick = jest.fn(func);
+  props.handleClick: mockOnClick;
   const portfolioitem = shallow(<PortfolioItem {...props} />);
   test('renders the component', () => {
     expect(portfolioitem.exists()).toBe(true);
   });
   test('onClick returns expected payload', () => {
-    const id = { id: '22'};
-    const func = () => props.handleClick(id);
-    const mockOnClick = jest.fn(func);
-    portfolioitem.setProps({handleClick: mockOnClick});
     portfolioitem.find('li').simulate('click');
     expect(mockOnClick.mock.results[0].value).toEqual(id);
   });
