77e5bf2bf3b3c0b465394c7e25f45fb89df343e2
james gladden <jamesgladden@gmail.com>
2018-10-15 18:29:02 -0700

    fix api domain typo

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const domain = 'http://farrung1.com';
+const domain = 'http://farrung.com';
 const api = `${domain}/api`;
 export const portfolioRequestUri = `${api}/allportfolio/`;
 export const authRequestUri = `${api}/authrequest/`;
