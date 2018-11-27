b572f62d58099b703c21209c41475d84d5c1d871
james gladden <jamesgladden@gmail.com>
2018-11-27 15:27:41 -0800

    restore authform on notauthorized page

--- src/components/Auth/NotAuthorized/index.js
+++ src/components/Auth/NotAuthorized/index.js
@@ -1,14 +1,13 @@
 import './styles.scss';
 import React from 'react';
 import AuthFormContainer from 'containers/Auth/FormContainer';
-import SignInContainer from 'containers/Auth/SignInContainer';
 
 const NotAuthorized = () => (
   <div id='notAuthorized'>
     <p id='notAuthorized__heading'>
     USER LOGIN:
     </p>
-    <SignInContainer />
+    <AuthFormContainer />
   </div>
 );
 
